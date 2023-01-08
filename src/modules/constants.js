b6a95e17a1cb86310c2cdc0fd80d1b163d874852
Vip <vwetoria@gmail.com>
2023-01-09 03:09:02 +0800

    增加类型匹配

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -1,24 +1,46 @@
 const GUDING = '固定'
 const LANGFEI = '废'
+
+const typeInCalender = {
+  GuDing: '固定',
+  LangFei: '废',
+  jiHuaWork: '计划-工作',
+  wetoria: 'Wetoria',
+  work: '工作',
+  family: '家庭',
+  selfDiscipline: '自律',
+  study: '学习',
+  friend: '朋友',
+  ShiYe: '事业',
+  '❤️': '❤️',
+}
+
+const defaultMap = {}
+Object.values(typeInCalender).forEach((i) => {
+  defaultMap[i] = i;
+})
+
 exports.scheduleTypeMap = {
-  "V":"Wetoria",
+  ...defaultMap,
+  "V": typeInCalender.wetoria,
   
-  "通勤": GUDING,
-  "午休": GUDING,
-  "固": GUDING,
-  "睡觉": GUDING,
+  "通勤": typeInCalender.GuDing,
+  "午休": typeInCalender.GuDing,
+  "固": typeInCalender.GuDing,
+  "睡觉": typeInCalender.GuDing,
 
-  "❤️":"❤️",
-  "家":"家庭",
-  "友":"朋友",
+  "❤️": typeInCalender['❤️'],
+  "家": typeInCalender.family,
+  "友": typeInCalender.friend,
 
-  "工作":"工作",
-  "事业":"事业",
+  "工作": typeInCalender.work,
+  "事业": typeInCalender.ShiYe,
 
-  "律":"自律",
-  "增":"学习",
+  "律": typeInCalender.selfDiscipline,
+  "增": typeInCalender.study,
+  "学习": typeInCalender.study,
 
-  "废": LANGFEI,
-  "玩": LANGFEI,
-  "摸鱼": LANGFEI,
+  "废": typeInCalender.LangFei,
+  "玩": typeInCalender.LangFei,
+  "摸鱼": typeInCalender.LangFei,
 }
